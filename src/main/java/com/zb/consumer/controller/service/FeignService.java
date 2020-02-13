8faa3fc2f8ccb693bd416c85cde7a4979397d178
1209506234 <1209506234@qq.com>
2020-02-13 16:15:16 +0800

    Initial commit

---
+++ src/main/java/com/zb/consumer/controller/service/FeignService.java
@@ -0,0 +1,17 @@
+package com.zb.consumer.controller.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
+
+@FeignClient(value = "SERVICE-PROVIDER", fallback = FeignHysticService.class)
+public interface FeignService {
+
+    @RequestMapping("/buy")
+    public String shopping();
+
+    @RequestMapping("/search")
+    public List<Map> search();
+}
